0fb5dd4e5f0aeb9f3bc9dcefec4c3133c7bc477b
Adam Mannino <amannino92@gmail.com>
2024-07-17 10:24:04 -0400

    Added features section to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Home.css';
 
+
+const featuresImage = './public/features.png';
+const featuresNav = './public/features_nav.png';
+
 const Home = () => {
     return (
         <div className="home-container">
@@ -11,11 +15,26 @@ const Home = () => {
                 <section className="intro">
                     <p className='emphasized-text'>Stay two steps ahead of the storm with</p>
                     <p className='spottr-text'>SPOT'R</p>
-                    <p className='smaller-text'>The ultimate tool for precise, real-time weather and data monitoring. All in one place.</p>
+                    <p className='smaller-text'>The ultimate tool for precise, real-time weather and data monitoring, all in one place.</p>
                     <div className="button-container">
                         <button className="trial-button">Start a Free Trial</button>
                         <button className="demo-button" onClick={() => window.location.href='/dashboard'}>Try a Live Demo</button>
                     </div>
+                </section>
+            </main>
+                <section className="our-software">
+                    <h2 className='software-header'>OUR SOFTWARE</h2>
+                    <h3 className='easy-powerful'>Easy, powerful software</h3>
+                    <h4 className='features-header'>Web dashboard app</h4>
+                    <ul className='features'>
+                        <li>Customizable to your organization’s policies, team structure, and more</li>
+                        <li>See any relevant risks today that could disrupt activities (e.g., heat, storms)</li>
+                        <li>Automated instructions for your team when weather disrupts</li>
+                        <li>Works with or without our on-site hardware</li>
+                        <li>Works with or without our on-site hardware</li>
+                    </ul>
+                    <img src={featuresImage} className='features-img' alt="features-image"/>
+                    <img src={featuresNav} alt="Features-Nav"/>
                 </section>
                 {/* <section className="features">
                     <h2>Features</h2>
@@ -26,7 +45,6 @@ const Home = () => {
                         <li>Customizable alerts</li>
                     </ul>
                 </section> */}
-            </main>
             <footer className="home-footer">
                 <p>&copy; 2024 SPOT'R. All rights reserved.</p>
             </footer>
