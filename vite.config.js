64c37a2935857620558cbae2bc1d37f90215f75c
Adam Mannino <amannino92@gmail.com>
2024-08-02 11:38:59 -0400

    Use public directory for fonts and ensure correct paths

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/assets/fonts/*',
-          dest: 'fonts'
-        }
-      ]
-    })
+    react()
   ],
   server: {
     proxy: {
